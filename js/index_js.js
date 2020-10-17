eafbbbc9900d93c30aa93de131b9971196539ff2
anminnuo <2413281965@qq.com>
2020-10-17 10:20:37 +0800

    first commit

---
+++ js/index_js.js
@@ -0,0 +1,11 @@
+/*
+Ðü¸¡½¥±ä
+ */
+function showDiv() {
+    var a = document.getElementById("bR");
+    a.style.display="block";
+}
+function hideDiv() {
+    var a = document.getElementById("bR");
+    a.style.display="none";
+}
